ec22f576c82c1ea73291ccacea657b2fbded23d3
sshao <sshao1126@gmail.com>
2022-10-23 15:13:39 -0700

    a6 react.js sp22 delete some file does not need

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Route, Routes} from "react-router";
 import './App.css';
-import './vendors/bootstrap/css/bootstrap.min.css';
+// import './vendors/bootstrap/css/bootstrap.min.css';
 import ExploreComponent from "./tuiter/explore/explore-component";
 import HomeScreen from "./tuiter/home/home-screen";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return(
@@ -23,18 +23,6 @@ function App() {
         </Routes>
       </div>
       </BrowserRouter>
-      // <BrowserRouter>
-      // <div className="container">
-      //   <h1>Welcome to WebDev</h1>
-      //   <Nav/>
-      //   <Routes>
-      //     <Route index element={<Labs/>}/>
-      //     <Route path="/hello" element={<h1>Hello</h1>}/>
-      //     <Route path="/tuiter" element={<Tuiter/>}/>
-      //   </Routes>
-      //   <h1>This is the footer</h1>
-      // </div>
-      // </BrowserRouter>
   );
 }
 export default App;
