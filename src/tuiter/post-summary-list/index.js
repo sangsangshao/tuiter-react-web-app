ec5a45b6152d54c8c25d02e88da9767bdf4909bf
sshao <sshao1126@gmail.com>
2022-11-18 16:00:08 -0800

    update post summary list

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, {useEffect} from "react";
 import posts from './posts.json';
 import PostSummaryItem from "./post-summary-item.js";
 import postsArray from './posts.json';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {findTuitsThunk} from "../../services/tuits-thunks";
 
 const PostSummaryList = () => {
-  const posts = useSelector(state => state.tuits)
+  // const posts = useSelector(state => state.tuits)
+  // return (
+  //     <>
+  //       <ul className="list-group wd-post-summary">
+  //         { posts?.map((post, index) => {
+  //           return (
+  //               <PostSummaryItem key={index} post={post}/>
+  //           );
+  //         })}
+  //       </ul>
+  //     </>
+  // )
+  const{tuits, loading} = useSelector(state => state.tuitsData)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(findTuitsThunk())
+  }, [dispatch])
   return (
-      <>
-        <ul className="list-group wd-post-summary">
-          { posts.map((post, index) => {
-            return (
-                <PostSummaryItem key={index} post={post}/>
-            );
-          })}
-        </ul>
-      </>
-  )
+    <ul className="list-group">
+      {
+        loading &&
+          <li className="list-group-item">
+            Loading...
+          </li>
+      }
+      {
+        tuits.map(post =>
+        <PostSummaryItem
+          key={post._id} post={post}/>)
+      }
+    </ul>
+  );
 }
 
 export default PostSummaryList;
