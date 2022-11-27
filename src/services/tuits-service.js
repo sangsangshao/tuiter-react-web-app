608c608a6575e811142c18d02a0df0e82638868a
sshao <sshao1126@gmail.com>
2022-11-27 11:36:53 -0800

    update api url

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-// const TUITS_API = 'http://localhost:4000/api/tuits';
+const TUITS_API = 'http://localhost:4000/api/tuits';
 // const TUITS_API = 'https://tuiter-node-server-app.herokuapp.com/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+// const API_BASE = process.env.REACT_APP_API_BASE;
+// const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
 
@@ -16,14 +16,14 @@ export const findTuits  = async ()     => {
   return tuits;
 }
 
-export const deleteTuit = async (tuit) => {
+export const deleteTuit = async (tid) => {
   const response = await axios
-  .delete(`${TUITS_API}/${tuit._id}`)
+  .delete(`${TUITS_API}/${tid}`)
   return response.data
 }
 
 export const updateTuit = async (tuit) => {
-  // const response =
-      await axios.put(`${TUITS_API}/${tuit._id}`, tuit);
+  const response = await axios
+  .put(`${TUITS_API}/${tuit._id}`, tuit);
   return tuit;
 }
